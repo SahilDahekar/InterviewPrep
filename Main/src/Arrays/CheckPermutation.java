c2aa4debe1851d5d851ca8d174a68ff9a8bd793f
SahilDahekar <dahekar30sahil@gmail.com>
2024-02-27 00:08:23 +0530

    Fixed permutation code

--- Main/src/Arrays/CheckPermutation.java
+++ Main/src/Arrays/CheckPermutation.java
@@ -6,6 +6,18 @@ public class CheckPermutation {
             return false;
         }
 
+        int[] count = new int[256];
+
+        for(int i = 0; i < s1.length(); i++){
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+
+        for(int n : count){
+            if(n != 0){
+                return false;
+            }
+        }
                 
         return true;
     }
