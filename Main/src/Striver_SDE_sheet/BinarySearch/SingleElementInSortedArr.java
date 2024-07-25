01c947e56b17f67c7adfb6ac5378b37a757b04ef
SahilDahekar <dahekar30sahil@gmail.com>
2024-07-25 23:53:55 +0530

    Solved 3 questions from strings & binarySearch

---
+++ Main/src/Striver_SDE_sheet/BinarySearch/SingleElementInSortedArr.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class SingleElementInSortedArr {
+    public static int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,4,4,8,8};
+        System.out.println(singleNonDuplicate(nums));
+    }
+}
